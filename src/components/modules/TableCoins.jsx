22b03b65c8ecfb1b42d8fe76d5140211d6d14482
MAGNET-CODE <sozan1382dastan@gmail.com>
2024-02-27 02:02:37 +0330

    add-table-coin

--- src/components/modules/TableCoins.jsx
+++ src/components/modules/TableCoins.jsx
@@ -1,10 +1,49 @@
-
+import chartUp from "../../assets/chart-up.svg";
+import chartDown from "../../assets/chart-down.svg";
 
 function TableCoins({ coins }) {
-  console.log(coins)
   return (
-    <div>TableCoins</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>Total volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            coins.map(coin => <TableRow coin={coin} key={coin.id} />)
+          }
+        </tbody>
+      </table>
+    </div>
   )
 }
 
 export default TableCoins
+const TableRow = ({ coin: {name, image, symbol, current_price, price_change_percentage_24h, total_volume} }) => {
+  return(
+            <tr>
+              <td>
+                <div>
+                  <img src={image} alt={name} />
+                  <span>{symbol.toUpperCase()}</span>
+                </div>
+              </td>
+              <td>{name}</td>
+              <td>${current_price.toLocaleString()}</td>
+              <td>{price_change_percentage_24h.toFixed(2)}%</td>
+              <td>{total_volume.toLocaleString()}</td>
+              <td>
+                <img src={
+                  price_change_percentage_24h > 0 ? chartUp : chartDown
+                } alt={name} />
+              </td>
+            </tr>
+  )
+}
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react"
 import { useEffect } from "react"
 import TableCoins from "../modules/TableCoins";
+import { getCoinList } from "../../services/cryptoApi"
 
-// may api key
-// CG-hj9aLi2n4LGPxLVrY3uy83X1
 function HomePage() {
     const [coins, setCoins] = useState([]);
     useEffect(() => {
-        fetch(
-            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en&x_cg_demo_api_key=CG-hj9aLi2n4LGPxLVrY3uy83X1"
-        )
-        .then(res => res.json())
-        .then(json => setCoins(json))
+      const getData = async () => {
+        const res = await fetch(getCoinList());
+        const json = await res.json();
+        setCoins(json)
+
+      };
+      getData();
     }, [])
   return (
     <div>
