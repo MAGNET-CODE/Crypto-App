8c7976ee5cc7048f8365837f3accf8beb843aa52
MAGNET-CODE <sozan1382dastan@gmail.com>
2024-02-27 15:54:47 +0330

    add-pagination

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -2,22 +2,27 @@ import { useState } from "react"
 import { useEffect } from "react"
 import TableCoins from "../modules/TableCoins";
 import { getCoinList } from "../../services/cryptoApi"
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
     const [coins, setCoins] = useState([]);
     const [isloading, setIsloading] = useState(true);
+    const [page, setPage] = useState(1);
+
     useEffect(() => {
       const getData = async () => {
-        const res = await fetch(getCoinList());
+        setIsloading(true);
+        const res = await fetch(getCoinList(page));
         const json = await res.json();
         setCoins(json);
         setIsloading(false);
       };
       getData();
-    }, [])
+    }, [page])
   return (
     <div>
         <TableCoins coins={coins} isloading={isloading} />
+        <Pagination page={page} setPage={setPage} />
     </div>
   )
 }
