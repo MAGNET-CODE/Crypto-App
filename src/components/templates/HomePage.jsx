91a19fffc8f0f8b0452a5743edf7ce16412eac0d
MAGNET-CODE <sozan1382dastan@gmail.com>
2024-02-28 04:32:21 +0330

    add-search

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -3,24 +3,31 @@ import { useEffect } from "react"
 import TableCoins from "../modules/TableCoins";
 import { getCoinList } from "../../services/cryptoApi"
 import Pagination from "../modules/Pagination";
+import Search from "../modules/Search";
 
 function HomePage() {
     const [coins, setCoins] = useState([]);
     const [isloading, setIsloading] = useState(true);
     const [page, setPage] = useState(1);
+    const [currency, setCurrency] = useState("usd");
 
     useEffect(() => {
       const getData = async () => {
-        setIsloading(true);
-        const res = await fetch(getCoinList(page));
-        const json = await res.json();
-        setCoins(json);
-        setIsloading(false);
+        try {
+          setIsloading(true);
+          const res = await fetch(getCoinList(page, currency));
+          const json = await res.json();
+          setCoins(json);
+          setIsloading(false);
+        } catch (error) {
+          console.log(error)
+        }
       };
       getData();
-    }, [page])
+    }, [page, currency])
   return (
     <div>
+        <Search currency={currency} setCurrency={setCurrency} />
         <TableCoins coins={coins} isloading={isloading} />
         <Pagination page={page} setPage={setPage} />
     </div>
